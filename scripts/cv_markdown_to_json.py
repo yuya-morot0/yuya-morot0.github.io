8fe33c816bbacb5c1d8faa8198ad871942206372
yuya-morot0 <moroto.research@gmail.com>
2025-05-11 17:29:22 +0900

    a

--- scripts/cv_markdown_to_json.py
+++ scripts/cv_markdown_to_json.py
@@ -132,12 +132,12 @@ def extract_author_info(config):
                 "url": author.get('researchgate')
             })
         
-        if author.get('dblp'):
-            profiles.append({
-                "network": "dblp",
-                "username": "",
-                "url": author.get('dblp')
-            })
+        # if author.get('dblp'):
+        #     profiles.append({
+        #         "network": "dblp",
+        #         "username": "",
+        #         "url": author.get('dblp')
+        #     })
         
         # Social media profiles
         if author.get('github'):
